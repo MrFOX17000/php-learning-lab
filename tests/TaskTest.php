d7d9763f2d674fdd3f8325875cf4589714504ceb
MrFOX17000 <renardmathias2@gmail.com>
2026-07-02 14:35:39 +0200

    test: add Task unit tests

---
+++ tests/TaskTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../src/Task.php';
+
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    public function testTaskCreation(): void
+    {
+        $task = new Task(1, 'Réviser PHP');
+
+        $this->assertSame(1, $task->getId());
+        $this->assertSame('Réviser PHP', $task->getTitle());
+    }
+
+    public function testNewTaskIsNotDone(): void
+    {
+        $task = new Task(2, 'Réviser SQL');
+
+        $this->assertFalse($task->isDone());
+    }
+
+    public function testTaskCanBeMarkedAsDone(): void
+    {
+        $task = new Task(3, 'Réviser JS');
+        $task->setDone();
+
+        $this->assertTrue($task->isDone());
+    }
+
+    public function testTaskRejectsEmptyTitle(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Task(4, '  ');
+    }
+
+    public function testTaskRejectsNegativeId(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Task(-1, 'Réviser CSS');
+    }
+
+    public function testTaskRejectsZeroId(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Task(0, 'Réviser CSS');
+    }
+}
